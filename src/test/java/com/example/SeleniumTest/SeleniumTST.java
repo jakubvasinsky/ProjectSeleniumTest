fb544f5a5d835af125696a156ad49ccdc79125c5
jakubvasinsky <jakub.vasinsky@gmail.com>
2021-01-26 16:02:47 +0100

    Automated - input fields, radio buttons, submit button

--- src/test/java/com/example/SeleniumTest/SeleniumTST.java
+++ src/test/java/com/example/SeleniumTest/SeleniumTST.java
@@ -1,8 +1,12 @@
 package com.example.SeleniumTest;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 
 public class SeleniumTST {
@@ -11,7 +15,54 @@ public class SeleniumTST {
         WebDriverManager.edgedriver().setup();
        WebDriver driver = new EdgeDriver();
 
-       driver.get("https://github.com/jakubvasinsky");
+       driver.get("http://itsovy.sk/testing/");
+        driver.manage().window().fullscreen();
+
+        WebElement polickoAmmount = driver.findElement(By.xpath("//*[@id=\"amount\"]"));
+
+        WebElement polickoInterest = driver.findElement(By.xpath("//*[@id=\"interest\"]"));
+
+        WebElement posuvadloPeriod = driver.findElement(By.xpath("//*[@id=\"period\"]"));
+
+        WebElement  labelPeriod = driver.findElement(By.id("lblPeriod"));
+
+        WebElement radioYes = driver.findElement(By.xpath("/html/body/div/form/table/tbody/tr[4]/td/input[1]"));
+        WebElement radioNo = driver.findElement(By.xpath("/html/body/div/form/table/tbody/tr[4]/td/input[2]"));
+
+        WebElement polickoAgree = driver.findElement(By.xpath("//*[@id=\"confirm\"]"));
+
+        WebElement btnCalculate = driver.findElement(By.id("btnsubmit"));
+
+        WebElement result = driver.findElement(By.id("result"));
+
+        //////////////////////////////////////////////////
+
+
+        polickoAmmount.sendKeys("111111");
+
+        polickoInterest.sendKeys("5");
+
+       /* Dimension sliderSize = posuvadloPeriod.getSize();
+        int sliderWidth = sliderSize.getWidth();
+        int xCoord = posuvadloPeriod.getLocation().getX();
+        Actions builder = new Actions(driver);
+        builder.moveToElement(posuvadloPeriod)
+                .click()
+                .dragAndDropBy
+                        (posuvadloPeriod,xCoord + sliderWidth, 0)
+                .build()
+                .perform();*/
+
+       /* posuvadloPeriod.getAttribute("value").equals("4");*/
+
+        radioYes.click();
+        radioYes.isSelected();
+        polickoAgree.click();
+        polickoAgree.isSelected();
+        btnCalculate.click();
+
+
+       /* driver.close();*/
 
     }
 
